6cb751c8f2e6637682d565da95e871dfdf609c2a
Brian Pilati <brian.pilati@domo.com>
2014-06-04 21:34:03 -0600

    udpates to fix a problem with the tests using npm test

--- tests/js/fixtures/karma.conf.js
+++ tests/js/fixtures/karma.conf.js
@@ -6,11 +6,9 @@ module.exports = function(config){
       'app/lib/**/angular.min.js',
       'app/lib/**/angular-route.min.js',
       'tests/js/lib/angular-mocks.min.js',
-      'app/lib/**/jquery.min.js',
-      'app/lib/**/underscore.min.js',
+      'app/lib/**/*.js',
       'app/js/**/*.js',
-      'tests/js/unit/**/*.js',
-      'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
+      'tests/js/unit/**/*.js'
     ],
 
     exclude : [
--- tests/js/fixtures/karma_auto_watch.conf.js
+++ tests/js/fixtures/karma_auto_watch.conf.js
@@ -5,11 +5,9 @@ module.exports = function(config){
     files : [
       'app/lib/**/angular.min.js',
       'app/lib/**/angular-route.min.js',
-      'app/lib/**/angular-animate.min.js',
       'tests/js/lib/angular-mocks.min.js',
       'app/lib/**/*.js',
       'app/js/**/*.js',
-      'app/partials/**/*.html',
       'tests/js/unit/**/*.js'
     ],
 
