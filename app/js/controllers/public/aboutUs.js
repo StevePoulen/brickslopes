160b801a9cfc76a89bc7eace8b2cba81beccbd2d
Brian Pilati <bpilati@insidesales.com>
2015-03-29 11:29:41 -0600

    updates for facebook adds

--- app/js/controllers/public/aboutUs.js
+++ app/js/controllers/public/aboutUs.js
@@ -2,4 +2,11 @@
 
 angular.module('Public').controller('aboutUs', ['$scope', '$route', function($scope, $route) {
     $scope.eventId = $route.current.params.eventId;
+
+    console.log($scope.eventId);
+
+    if ($scope.eventId == '22') {
+        $scope.imageUrl = "/images/emails/images/t-shirt-2015.png";
+    }
 }]);
+
