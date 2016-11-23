c32073b1e9f169bd6602c4f61b0856efe8629fc3
Brian Pilati <brian@homie.com>
2016-11-22 18:05:56 -0700

    Fixed the config_template

--- config/config_template.php
+++ config/config_template.php
@@ -1,11 +1,13 @@
 <?php
     #Information for testing on a local environment 
     //Copy this from config_template.php to config.php
+    define('CURRENT_EVENT', 4);
+    define('SITE', '/path/to/brickslopes/directory/');
     define('DB_HOST', '127.0.0.1');
     define('DB_PASSWD', '<local_db_passwd>');
     define('LOGMASK', 1);
-    define('DB_USER', 'brickslopes');
-    define('DB_NAME', 'brickslopes');
+    define('DB_USER', '<local_db_user>');
+    define('DB_NAME', '<local_db_name>');
     define('JWT_KEY', 'mykey');
     define('WEBSITE', 'mybrickslopes.com');
     define('EMAIL_CLIENT_ID', '<your_gmail_client_id>');
